554015b1d2c9874327d5d794a38459d412b353a4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-02 02:04:27 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -2,7 +2,7 @@ from userbot import *
 from userbot.Config import Config
 from userbot.helpers.functions import *
 from userbot.cmdhelp import CmdHelp
-from AuraXBot.utils import *
+from Aniebot.utils import *
 from telethon import events, version
 from telethon.events import NewMessage
 from telethon.tl.custom import Dialog
@@ -13,7 +13,7 @@ from telethon.tl.types import Channel, Chat, User
 # =================== CONSTANT ===================
 
 USERID = bot.uid
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "AuraX User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Anie User"
 # mention user
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
