cb49a8359714d82301156556757ae80b3cb37881
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-04 16:15:46 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
+
+
 echo """
-    Anieuserbot
+___  ___                   
+|  \/  |                   
+| .  . | ___  _____      __
+| |\/| |/ _ \/ _ \ \ /\ / /
+| |  | |  __/ (_) \ V  V / 
+\_|  |_/\___|\___/ \_/\_/  
+                           
+                           
 """
-python3 -m Anie-Userbot 
+
+python3 -m Meowbot
