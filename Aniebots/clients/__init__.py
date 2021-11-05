d38bb1fc6637eb929424af18c58a3016705b2c26
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-05 23:16:13 +0530

    Update __init__.py

--- Aniebots/clients/__init__.py
+++ Aniebots/clients/__init__.py
@@ -1,3 +1,3 @@
 from .client_list import clients_list, client_id
 from .decs import mew_cmd, mew_handler
-from .session import mew H2,H3,H4,H5, Aniebots 
+from .session import mew, H2, H3, H4, H5, Aniebots 
