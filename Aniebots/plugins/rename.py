78209c2083f00c81f6eb36fe33c7792c04558b93
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-05 08:50:10 +0530

    Update rename.py

--- Aniebots/plugins/rename.py
+++ Aniebots/plugins/rename.py
@@ -7,7 +7,7 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
 
-from . import *
+from Aniebots. import *
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
