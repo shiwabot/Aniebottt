b0fc5ece950e1e62d1afcad93585336a1a8b173d
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-05 13:08:00 +0530

    Update create.py

--- Aniebots/plugins/create.py
+++ Aniebots/plugins/create.py
@@ -1,7 +1,7 @@
 from telethon.tl import functions
 from telethon.tl.types import MessageEntityMentionName
 
-from Aniebots. import *
+from Aniebots import *
 
 
 @bot.on(mew_cmd(pattern="create (b|g|c) (.*)"))  # pylint:disable=E0602
