56ed527f5e0425bbff8d92185457d7214dfb1d90
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-05 13:10:00 +0530

    Update echo.py

--- Aniebots/plugins/echo.py
+++ Aniebots/plugins/echo.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.messages import ImportChatInviteRequest as Get
 
 from Aniebots.sql.echo_sql import addecho, get_all_echos, is_echo, remove_echo
 
-from Aniebots. import *
+from Aniebots import *
 
 
 @bot.on(admin_cmd(pattern="echo$"))
