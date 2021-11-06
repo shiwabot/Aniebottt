a6251b89c4c1e6c9805dd60f2a45d71eb38ec091
「 Rajkumar™ 」 <71130660+Awesome-RJ@users.noreply.github.com>
2021-11-06 15:54:45 +0530

    Update decorators.py

--- Aniebots/utils/decorators.py
+++ Aniebots/utils/decorators.py
@@ -5,12 +5,6 @@ from pathlib import Path
 from telethon import events
 
 from Aniebots import *
-
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
-from Aniebots import CmdHelp
-
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
-from Aniebots import CmdHelp
 from Aniebots.config import Config
 from Aniebots.helpers import *
 
