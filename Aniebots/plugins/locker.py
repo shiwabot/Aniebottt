eb49eb14b7558473eb84b62275af1f322ae63122
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-07 01:00:49 +0530

    Update locker.py

--- Aniebots/plugins/locker.py
+++ Aniebots/plugins/locker.py
@@ -2,7 +2,7 @@ from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
 from telethon.tl.types import ChatBannedRights
 
 from Aniebots import *
-from Aniebots import CmdHelp
+from Aniebots.cmdhelp import CmdHelp
 
 
 @bot.on(mew_cmd(pattern=r"lock ?(.*)", outgoing=True))
