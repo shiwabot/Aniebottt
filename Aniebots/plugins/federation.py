2e0936da29477e6af21db02b7fa675f14fe2e982
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-06 21:42:31 +0530

    Update federation.py

--- Aniebots/plugins/federation.py
+++ Aniebots/plugins/federation.py
@@ -4,7 +4,6 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from Aniebots import *
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
 from Aniebots import CmdHelp
 
 logs_id = Config.FBAN_LOG_GROUP
