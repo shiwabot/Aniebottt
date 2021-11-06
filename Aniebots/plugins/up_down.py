6a1f045de042e0e482981be000387cdd0188d312
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-06 22:08:18 +0530

    Update up_down.py

--- Aniebots/plugins/up_down.py
+++ Aniebots/plugins/up_down.py
@@ -13,7 +13,6 @@ from pySmartDL import SmartDL
 from telethon.tl.types import DocumentAttributeVideo
 
 from Aniebots import *
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
 from Aniebots import CmdHelp
 
 
