f8e3973655ac467c394539866ba938cb9ed2a87a
Amaan The Gamer King <73664034+Javes786@users.noreply.github.com>
2021-11-06 23:20:40 +0530

    Update pasters.py

--- Aniebots/helpers/pasters.py
+++ Aniebots/helpers/pasters.py
@@ -4,7 +4,7 @@ import requests
 
 from Aniebots import *
 
-from Aniebots import CmdHelp
+# DoNe :/
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36",
