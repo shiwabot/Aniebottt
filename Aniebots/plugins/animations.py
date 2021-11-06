3bc161e66dfeddf5d906e1fbe540fcef40facaad
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-07 00:46:59 +0530

    Update animations.py

--- Aniebots/plugins/animations.py
+++ Aniebots/plugins/animations.py
@@ -2,7 +2,7 @@ import asyncio
 from collections import deque
 
 from Aniebots import *
-from Aniebots import CmdHelp
+from Aniebots.cmdhelp import CmdHelp
 
 
 @bot.on(mew_cmd(pattern=r"boxs$", outgoing=True))
