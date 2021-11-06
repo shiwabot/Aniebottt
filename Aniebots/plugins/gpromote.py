9ac318ac4a2c15cc46a793ce3dd32a8033f20267
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-06 21:48:15 +0530

    Update gpromote.py

--- Aniebots/plugins/gpromote.py
+++ Aniebots/plugins/gpromote.py
@@ -2,7 +2,6 @@ from telethon.tl.functions.channels import EditAdminRequest
 from telethon.tl.types import ChatAdminRights, MessageEntityMentionName
 
 from Aniebots import *
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
 from Aniebots import CmdHelp
 
 
