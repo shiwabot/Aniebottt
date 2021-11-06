bf01d9a0f1fa50bf12c594a842f24ef5e4539b83
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-06 21:44:31 +0530

    Update fsub.py

--- Aniebots/plugins/fsub.py
+++ Aniebots/plugins/fsub.py
@@ -4,7 +4,6 @@ from telethon.tl.functions.channels import GetParticipantRequest
 from Aniebots.sql.fsub_sql import *
 
 from Aniebots import *
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
 from Aniebots import CmdHelp
 
 """
