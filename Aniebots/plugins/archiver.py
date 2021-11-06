2ecd2245d4a9c5cc5b2ee73ff843ca5c815799b4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-06 21:28:41 +0530

    Update archiver.py

--- Aniebots/plugins/archiver.py
+++ Aniebots/plugins/archiver.py
@@ -12,7 +12,6 @@ from hachoir.parser import createParser
 from telethon.tl.types import DocumentAttributeVideo
 
 from Aniebots import *
-from Aniebots.utils.decorators import mew_cmd, sudo_cmd
 from Aniebots import CmdHelp
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
