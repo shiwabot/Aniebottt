82c59b5c93214d742ee4741bd0005868ac8700b7
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 01:56:54 +0530

    Update groupdata.py

--- userbot/plugins/groupdata.py
+++ userbot/plugins/groupdata.py
@@ -20,7 +20,7 @@ from telethon.tl.types import ChannelParticipantsAdmins, MessageActionChannelMig
 from telethon.utils import get_input_location
 
 from userbot import CMD_HELP
-from mafiabot.utils import admin_cmd, errors_handler, sudo_cmd, edit_or_reply
+from Aniebot.utils import admin_cmd, errors_handler, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
