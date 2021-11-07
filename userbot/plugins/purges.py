5304b99843c5e5e415a45846e73950417a99a019
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 02:36:23 +0530

    Update purges.py

--- userbot/plugins/purges.py
+++ userbot/plugins/purges.py
@@ -2,7 +2,7 @@ import asyncio
 import re
 import telethon.utils
 from telethon import events
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from Aniebot.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
