ceedf551eecdc6b285bb53f9edcf97bccc91a7f4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 01:31:18 +0530

    Update execmod.py

--- userbot/plugins/execmod.py
+++ userbot/plugins/execmod.py
@@ -9,7 +9,7 @@ import time
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
 
-from mafiabot.utils import admin_cmd
+from Aniebot.utils import admin_cmd
 from userbot.cmdhelp import CmdHelp
 
 if not os.path.isdir("./SAVED"):
