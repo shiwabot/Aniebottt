a2735f65eec2bccb2b9da542bca886ef96b60394
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 00:21:57 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -160,7 +160,7 @@ if ENV:
         FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
         EXTRA_MAFIABOT = os.environ.get("EXTRA_MAFIABOT", -1001221881562)
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
-        MAFIABOT_LOGGER = int(os.environ.get("MAFIABOT_LOGGER", None))
+        ANIEBOTS_LOGGER = int(os.environ.get("ANIEBOTS_LOGGER", None))
         BAN_PIC = os.environ.get("BAN_PIC", None)
         ABUSE = os.environ.get("ABUSE", None)
 else:
