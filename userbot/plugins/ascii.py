1d67ad0026a8acb86dbf0848fdb4d8a9856b2672
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 01:13:09 +0530

    Update ascii.py

--- userbot/plugins/ascii.py
+++ userbot/plugins/ascii.py
@@ -8,12 +8,12 @@ from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 
 from userbot import CMD_HELP
-from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
+from Aniebot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 from userbot.Config import Config
 from . import *
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Anie User"
 
 USERID = bot.uid
 
