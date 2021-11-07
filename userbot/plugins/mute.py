8782f265edacd21db09ac2c256bc3b6d097924d4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 02:17:24 +0530

    Update mute.py

--- userbot/plugins/mute.py
+++ userbot/plugins/mute.py
@@ -2,7 +2,7 @@
 """
 import asyncio
 
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from Aniebot.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 from userbot.plugins.sql_helper.mute_sql import is_muted, mute, unmute
 
