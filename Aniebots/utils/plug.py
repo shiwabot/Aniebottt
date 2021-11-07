7415cf9bd5b0f0fd32ae8c68c4195b8868046097
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-07 14:03:04 +0530

    Add files via upload

---
+++ Aniebots/utils/plug.py
@@ -0,0 +1,88 @@
+import importlib
+import logging
+import os
+import sys
+from pathlib import Path
+
+from Aniebots import *
+
+from Aniebots.config import *
+from Aniebots.helpers import *
+from Aniebots.utils import *
+
+# ENV
+ENV = bool(os.environ.get("ENV", False))
+if ENV:
+    from Aniebots.config import Config
+else:
+    if os.path.exists("Config.py"):
+        from Config import Development as Config
+
+
+# load plugins
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import Aniebots.utils
+
+        path = Path(f"Aniebots/plugins/{shortname}.py")
+        name = "Aniehots.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("Aniebots - Successfully imported " + shortname)
+    else:
+        import Aniebots.utils
+
+        path = Path(f"Aniebots/plugins/{shortname}.py")
+        name = "Aniebots.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = bot
+        mod.tgbot = bot.tgbot
+        mod.command = command
+        mod.logger = logging.getLogger(shortname)
+        # support for uniborg
+        sys.modules["uniborg.util"] = Aniebots.utils
+        mod.Config = Config
+        mod.borg = bot
+        mod.Aniebots = bot
+        mod.edit_or_reply = edit_or_reply
+        mod.eor = edit_or_reply
+        mod.delete_mew = delete_mew
+        mod.eod = delete_mew
+        mod.Var = Config
+        mod.mew_cmd = mew_cmd
+        mod.sudo_cmd = sudo_cmd
+        # support for other userbots
+        sys.modules["userbot.utils"] = Aniebots.utils
+        sys.modules["userbot"] = Aniebots 
+        # support for paperplaneextended
+        sys.modules["userbot.events"] = Aniebots 
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["Aniebots.plugins." + shortname] = mod
+        LOGS.info("⚡ 🄰🄽🄸🄴-🅄🅂🄴🅁🄱🄾🅃 ⚡ - Successfully Imported " + shortname)
+
+
+# remove plugins
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in LOAD_PLUG[shortname]:
+                bot.remove_event_handler(i)
+            del LOAD_PLUG[shortname]
+
+        except BaseException:
+            name = f"Aniebots.plugins.{shortname}"
+
+            for i in reversed(range(len(bot._event_builders))):
+                ev, cb = bot._event_builders[i]
+                if cb.__module__ == name:
+                    del bot._event_builders[i]
+    except BaseException:
+        raise ValueError
+
+
+# Aniebot
