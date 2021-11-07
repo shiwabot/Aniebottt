2383f456e23434a068a54b6503b17ae563888191
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 01:11:46 +0530

    Update archive.py

--- userbot/plugins/archive.py
+++ userbot/plugins/archive.py
@@ -19,7 +19,7 @@ from telethon.tl.types import DocumentAttributeVideo
 
 from userbot import CMD_HELP
 from userbot.Config import Config
-from mafiabot.utils import admin_cmd, progress
+from Aniebot.utils import admin_cmd, progress
 from userbot.cmdhelp import CmdHelp
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
