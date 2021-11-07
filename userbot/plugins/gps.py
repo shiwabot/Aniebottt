208d02dfa684ac66a40c140939976799fe523420
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 01:56:16 +0530

    Update gps.py

--- userbot/plugins/gps.py
+++ userbot/plugins/gps.py
@@ -5,8 +5,8 @@
 from geopy.geocoders import Nominatim
 from telethon.tl import types
 
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
-from mafiabot import CmdHelp
+from Aniebot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from Aniebot import CmdHelp
 
 @bot.on(admin_cmd(pattern="gps ?(.*)"))
 @bot.on(sudo_cmd(pattern="gps ?(.*)", allow_sudo=True))
@@ -23,7 +23,7 @@ async def gps(event):
 
     await edit_or_reply(event, "Finding😁")
 
-    geolocator = Nominatim(user_agent="mafiabot")
+    geolocator = Nominatim(user_agent="aniebot")
     geoloc = geolocator.geocode(input_str)
 
     if geoloc:
