250cf6c19455898b919b64794a955425f0673bc8
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 02:06:19 +0530

    Update json.py

--- userbot/plugins/json.py
+++ userbot/plugins/json.py
@@ -2,7 +2,7 @@
 Syntax: .json"""
 import io
 
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from Aniebot.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
