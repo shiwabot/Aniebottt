699b6d4507f15c0ccd078a42d6ff1f493156db54
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 16:00:04 +0530

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -3,7 +3,7 @@ from telethon.sync import TelegramClient
 mafia = input("✵ Enter y/yes to continue: ")
 if mafia == 'y' or 'yes':
  print("\nPlease go to my.telegram.org and get your API Id and API Hash to proceed\n\n 𒊹︎︎︎ɢɪᴛʜᴜʙ ʀᴇᴘᴏ ɪs ➪➪ https://github.com/MafiaBotOP/MafiaBot")
-print("""\n\nWelcome To MafiaBot String Session\nGenerator By @H1M4N5HU0P\n\n""")
+print("""\n\nWelcome To Anie String Session\nGenerator By @d3nvil \n\n""")
 print("""Enter Your Valid Details To Continue!\n\n """)
 
 API_KEY = input("API_ID:  ")
@@ -19,11 +19,11 @@ while True:
             session = client.session.save()
             client.send_message(
                 "me",
-                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @MafiaBot_Support For Any Help!\n\n",
+                f"Here is your TELEGRAM STRING SESSION\n(Tap to copy it)👇 \n\n `{session}` \n\n And Visit @Aniebotsupports For Any Help!\n\n",
             )
 
             print(
-                "Thanks for Choosing MafiaBot Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
+                "Thanks for Choosing Anie Have A Good Time....Note That When You Terminate the Old Session ComeBack And Genrate A New String Session Old One Wont Work"
             )
     except:
         print("")
