e7852bf3fecfa864c6b2fce61ae318d67e0ec012
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-08 09:50:40 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -151,7 +151,7 @@ except:
     # Upstream Repo
     UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/MafiaBotOP/MafiaBot.git")
+    "https://github.com/Anieteam/Aniebots.git")
 
     # Last.fm Module
     BIO_PREFIX = os.environ.get("BIO_PREFIX", None)
