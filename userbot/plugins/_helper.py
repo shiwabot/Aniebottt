e8644f8b833dc34b01908555e17f674f06ddf0e4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-09 02:06:28 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -16,7 +16,7 @@ async def _(event):
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or mafia_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@MafiaBot_Support")
+        results = await event.client.inline_query(tgbotusername, "@Aniebotsupports")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
@@ -64,7 +64,7 @@ async def info(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the MafiaBot are [here]({url})"
+            reply_text = f"All commands of the Aniebot are [here]({url})"
             await event.reply(reply_text, link_preview=False)
             return
         await event.reply(
