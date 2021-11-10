64f1a0439e1de051ab0be9139b49b6680381d578
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-11 02:01:41 +0530

    Update gbun.py

--- userbot/plugins/gbun.py
+++ userbot/plugins/gbun.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from userbot.utils import admin_cmd
+from Aniebot.utils import admin_cmd
 
 
 @borg.on(admin_cmd("gbun"))
