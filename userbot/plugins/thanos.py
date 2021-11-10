bc83783eccb34de15bf2af228325131191503bb6
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-11 02:26:54 +0530

    Update thanos.py

--- userbot/plugins/thanos.py
+++ userbot/plugins/thanos.py
@@ -1,7 +1,7 @@
 import asyncio
 
 from telethon import events
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from Aniebot.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 @bot.on(admin_cmd(pattern="thanos$", outgoing=True))
 @bot.on(sudo_cmd(pattern="thanos$", allow_sudo=True))
