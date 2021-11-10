edeae0d0c725104017536738b4e94e999123fccb
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-10 09:48:16 +0530

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,53 +1,117 @@
+# For @TeleBotHelp
+"""Check if your userbot is working."""
+import time
+from datetime import datetime
+from io import BytesIO
 
-# Thanks to @D3_krish
-# Porting in boy by @d3nvil 
+import requests
+from PIL import Image
 
-import asyncio
-import random
-from telethon import events, version
-from userbot import mafiaversion
-from userbot.utils import admin_cmd, sudo_cmd
-from telethon.tl.types import ChannelParticipantsAdmins
-from userbot.cmdhelp import CmdHelp
-from userbot.Config import Config
-from . import *
-# 🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔🤔
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "🄰🄽🄸🄴-🅄🅂🄴🅁🄱🄾🅃"
+from telebot import ALIVE_NAME, CMD_HELP, telever
+from telebot.__init__ import StartTime
+from telebot.telebotConfig import Config, Var
 
-ludosudo = Config.SUDO_USERS
-
-if ludosudo:
-    sudou = "True"
-else:
-    sudou = "False"
-
-Anie = bot.uid
-
-ANIE_IMG = Config.ALIVE_PIC or "https://telegra.ph/file/28c979a7a36c344da3e07.jpg"
-pm_caption = "  __**🔥🔥 𝐁𝐎𝐓 𝐈𝐒 𝐀𝐋𝐈𝐕𝐄🔥🔥**__\n\n"
-
-pm_caption += f"**━━━━━━━━━━━━━━━━━━━━**\n\n"
-pm_caption += (
-    f"                 👑𝐌𝐀𝐒𝐓𝐄𝐑👑\n  **『😈[{DEFAULTUSER}](tg://user?id={Anie})😈』**\n\n"
+# ======CONSTANTS=========#
+CUSTOM_ALIVE = (
+    Var.CUSTOM_ALIVE
+    if Var.CUSTOM_ALIVE
+    else "Hey! I'm alive. All systems online and functioning normally!"
 )
-pm_caption += f"┏━━━━━━━━━━━━━━━━━━━\n"
-pm_caption += f"┣•➳➠ `Telethon:` `{version.__version__}` \n"
-pm_caption += f"┣•➳➠ `Version:` `{mafiaversion}`\n"
-pm_caption += f"┣•➳➠ `Sudo:` `{sudou}`\n"
-pm_caption += f"┣•➳➠ `Channel:` [🄰🄽🄸🄴](https://t.me/Aniebots)\n"
-pm_caption += f"┣•➳➠ `Creator:` [d3nvil](https://t.me/d3nvil)\n"
-pm_caption += f"┗━━━━━━━━━━━━━━━━━━━\n"
-pm_caption += " [🔥REPO🔥](https://github.com/Anieteam/Aniebots) 🔹 [📜License📜](https://github.com/Anieteam/Aniebots/blob/main/LICENSE)"
+ALV_PIC = Var.ALIVE_PIC if Var.ALIVE_PIC else None
+telemoji = Var.CUSTOM_ALIVE_EMOJI if Var.CUSTOM_ALIVE_EMOJI else "**✵**"
+if Config.SUDO_USERS:
+    sudo = "Enabled"
+else:
+    sudo = "Disabled"
+# ======CONSTANTS=========#
 
-# @command(outgoing=True, pattern="^.alive$")
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "@TeleBotSupport"
+
+
+@telebot.on(admin_cmd(outgoing=True, pattern="alive"))
+@telebot.on(sudo_cmd(outgoing=True, pattern="alive", allow_sudo=True))
 async def amireallyalive(alive):
-    await alive.get_chat()   
-    await alive.delete()
-    on = await borg.send_file(alive.chat_id, ANIE_IMG,caption=pm_caption)
+    start = datetime.now()
+    myid = bot.uid
+    """ For .alive command, check if the bot is running.  """
+    end = datetime.now()
+    (end - start).microseconds / 1000
+    uptime = get_readable_time((time.time() - StartTime))
+    if ALV_PIC:
+        tele = f"**Welcome To TeleBot **\n\n"
+        tele += f"`{CUSTOM_ALIVE}`\n\n"
+        tele += (
+            f"{telemoji} **Telethon version**: `1.17`\n{telemoji} **Python**: `3.8.3`\n"
+        )
+        tele += f"{telemoji} **TeleBot Version**: `{telever}`\n"
+        tele += f"{telemoji} **More Info**: @TeleBotSupport\n"
+        tele += f"{telemoji} **Sudo** : `{sudo}`\n"
+        tele += f"{telemoji} **TeleBot Uptime**: `{uptime}`\n"
+        tele += f"{telemoji} **Database Status**: `All OK 👌!`\n"
+        tele += (
+            f"{telemoji} **My pro owner** : [{DEFAULTUSER}](tg://user?id={myid})\n\n"
+        )
+        tele += "    [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)"
+        await alive.get_chat()
+        await alive.delete()
+        """ For .alive command, check if the bot is running.  """
+        await borg.send_file(alive.chat_id, ALV_PIC, caption=tele, link_preview=False)
+        await alive.delete()
+        return
+    req = requests.get("https://telegra.ph/file/0670190de8e3bddea6d95.png")
+    req.raise_for_status()
+    file = BytesIO(req.content)
+    file.seek(0)
+    img = Image.open(file)
+    with BytesIO() as sticker:
+        img.save(sticker, "webp")
+        sticker.name = "sticker.webp"
+        sticker.seek(0)
+        await borg.send_message(
+            alive.chat_id,
+            f"**Welcome To TeleBot **\n\n"
+            f"`{CUSTOM_ALIVE}`\n\n"
+            f"{telemoji} **Telethon version**: `1.17`\n{telemoji} **Python**: `3.8.3`\n"
+            f"{telemoji} **TeleBot Version**: `{telever}`\n"
+            f"{telemoji} **More Info**: @TeleBotSupport\n"
+            f"{telemoji} **Sudo** : `{sudo}`\n"
+            f"{telemoji} **TeleBot Uptime**: `{uptime}`\n"
+            f"{telemoji} **Database Status**: `All OK 👌!`\n"
+            f"{telemoji} **My pro owner** : [{DEFAULTUSER}](tg://user?id={myid})\n\n"
+            "    [✨ GitHub Repository ✨](https://github.com/xditya/TeleBot)",
+            link_preview=False,
+        )
+        await borg.send_file(alive.chat_id, file=sticker)
+        await alive.delete()
 
-    
-CmdHelp("alive").add_command(
-  "alive", None, "To check am i alive"
-).add()
+
+CMD_HELP.update({"alive": "➟ `.alive`\nUse - Check if your bot is working."})
