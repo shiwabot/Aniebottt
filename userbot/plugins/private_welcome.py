a25c3c25841bfcea14e9ef6163f00683702af857
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-16 21:37:27 +0530

    Update private_welcome.py

--- userbot/plugins/private_welcome.py
+++ userbot/plugins/private_welcome.py
@@ -1,6 +1,6 @@
 from telethon import events
 
-from firebot import BOTLOG_CHATID, CMD_HELP, bot, pro
+from userbot import BOTLOG_CHATID, CMD_HELP, bot, pro
 
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 from .sql_helper import pmpermit_sql as pmpermit_sql
