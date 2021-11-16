fc12d0aba4c6ab86dbd45818e48a98af80c0c3d4
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-16 15:48:12 +0530

    Update bsdk.py

--- userbot/plugins/bsdk.py
+++ userbot/plugins/bsdk.py
@@ -3,7 +3,7 @@
 
 import random
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
-from beastx import ALIVE_NAME, CMD_HELP
+from userbot import ALIVE_NAME, CMD_HELP
 from . import *
 """
 ✘ Commands Available -
@@ -12,7 +12,7 @@ from . import *
 """
 
 bsdk = ["Phatele Nirodh Ke Natije!","Chut Ka Maindak…","Abla Naari, Tere Bable Bhaari…","Chut Ke Pasine Mein Talay Hue Bhajiye…","Chullu Bhar Muth Mein Doob Mar!","Kaali Chut Ke Safed Jhaant…","Gote Kitne Bhi Badey Ho, Lund Ke Niche Hi Rehtein Hain…","Naa Chut, Naa Choche, Aur Nakhre Noor Jahan Ke!","Teri Gaand Mein Kutte Ka Lund…","Teri Jhaatein Kaat Kar Tere Mooh Par Laga Kar Unki French Beard Bana Doonga!"]
-@beast.on(admin_cmd(pattern="bsdk$"))
+@bot.on(admin_cmd(pattern="bsdk$"))
 async def thanks(ult):
   b = random.choice(bsdk)
   return await ult.edit(b)
