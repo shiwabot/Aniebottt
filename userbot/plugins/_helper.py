54620f961fa6ff3129539a8c99156281c606517c
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-18 13:09:20 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -14,8 +14,8 @@ async def yardim(event):
         return
     tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
     input_str = event.pattern_match.group(1)
-    if tgbotusername is not None or Anie_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@Aniebots")
+    if tgbotusername is not None or REBEL_input == "text":
+        results = await event.client.inline_query(tgbotusername, "@Anieuserbot")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
