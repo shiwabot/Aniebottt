178463015b9721eeff913a649e4a77d4a85c367b
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-19 11:19:34 +0530

    Update sticker.py

--- userbot/plugins/sticker.py
+++ userbot/plugins/sticker.py
@@ -55,7 +55,7 @@ async def _(event):
     me = borg.me
     userid = event.sender_id
     packname = f"{STICKER_PACK_NAME}"
-    packshortname = f"{tg://user?id={}}_Pack"  # format: Uni_Borg AliveName
+    packshortname = "Anieuserbot_animated"  # format: Uni_Borg AliveName
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "@Anieuserbot_Sticker.png"
