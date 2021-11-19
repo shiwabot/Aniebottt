a236a09730d34ec40718df7b6c542199b6d5e4c6
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-11-19 10:38:35 +0530

    Add files via upload

---
+++ userbot/exclusive.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2020 by rohithaditya@Github, < https://github.com/rohithaditya/Godhackerzuserbot >.
+# Copyright © Rohithaditya
+# This file is part of < https://github.com/rohithaditya/Godhackerz-userbot/blob/main/LICENSE > project,
+# and is released under the "GNU v2.0 License Agreement".
+# Please see < https://github.com/rohithaditya/Godhackerz-userbot/blob/main/LICENSE >
+#
+# All rights reserved.
+
+import os
+
+STICKER_PACK_NAME = os.environ.get("STICKER_PACK_NAME", "Godhackerzuserbot_Stickers_kang")
+
+ANIMATED_STICKER_PNAME = os.environ.get("ANIMATED_STICKER_PNAME", "Godhackerzuserbot_Animated_Stickers")
+
+AUTONAME = os.environ.get("AUTONAME", None)
+
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/Godhackerzuserbot/17")
+
+ALIVE_NAME = os.environ.get("ALIVE_NAME", None)
+
+# © @rohithaditya
+# © @Godhackerzuserbot
