29d9f5c22f067bad8aa3b06a32a978018731f0de
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-12-10 18:19:06 +0530

    Update repo.py

--- userbot/plugins/repo.py
+++ userbot/plugins/repo.py
@@ -2,15 +2,15 @@
 # updated by madboy482
 # updated by madboy482
 from telethon import events, Button, custom
-from ULTRA import bot
-from ULTRAX import xbot
+from userbot import bot
+from userbot import bot
 # updated by madboy482
 # updated by madboy482
 # updated by madboy482
 import os,re
 from telethon.tl.custom import Button 
 from telethon import events, errors, custom, functions
-@xbot.on(events.InlineQuery(pattern=r"repo"))
+@bot.on(events.InlineQuery(pattern=r"repo"))
 async def inline_id_handler(event: events.InlineQuery.Event):
  # updated by madboy482
  # updated by madboy482
@@ -21,7 +21,7 @@ async def inline_id_handler(event: events.InlineQuery.Event):
  # updated by madboy482
  # updated by madboy482
  # updated by madboy482
- result = LEGENDX.article("UʟᴛʀᴀX",text="**UltraX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @UltraXOT**",buttons=X,link_preview=False)
+ result = LEGENDX.article("UʟᴛʀᴀX",text="**Anie's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Sᴜᴘᴘᴏʀᴛ\n\n© @Aniebots**",buttons=X,link_preview=False)
  # updated by madboy482
  # updated by madboy482
  # updated by madboy482
@@ -31,22 +31,22 @@ async def inline_id_handler(event: events.InlineQuery.Event):
 # updated by madboy482
 # updated by madboy482
 async def callback_query_handler(event):
-# inline by TEAMLEGEND, TEAMULTRAX
-  await event.edit(text=f"**UʟᴛʀᴀX's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @UltraXOT**",buttons=[
+# inline by ANIETEAM, TEAMULTRAX
+  await event.edit(text=f"**ANIE's Rᴇᴘᴏ, Dᴇᴘʟᴏʏ ᴀɴᴅ Gʀᴏᴜᴘ Lɪɴᴋ\n\n© @ANIEBOTS**",buttons=[
    # updated by madboy482
    # updated by madboy482
    # updated by madboy482
                 [
-                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/ULTRA-OP/ULTRA-X"),
+                    Button.url(f"⚜️ Rᴇᴘᴏ ⚜️", url="https://github.com/Anieteam/Aniebots"),
                  # updated by madboy482
                  # updated by madboy482
                  # updated by madboy482
-                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/ULTRAXCHAT")],
+                    Button.url(f"🌚 Sᴜᴘᴘᴏʀᴛ Cʜᴀᴛ 🌝", url="https://t.me/Aniebotsupports")],
    # updated by madboy482
    # updated by madboy482
    # updated by madboy482
-                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FULTRA-OP%2FHEROKU"),
-                     Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@legendx22/ULTRA-X#main.py"),
+                    [Button.url(f"🔰 Dᴇᴘʟᴏʏ 🔰", url="https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FAnieteam%2FHEROKU&template=https%3A%2F%2Fgithub.com%2FAnieteam%2FHEROKU"),
+                     Button.url(f"💠 Sᴛʀɪɴɢ 💠", url="https://replit.com/@Denvilop/Aniebots#main.py"),
                 ]
             ]
                    # updated by madboy482
