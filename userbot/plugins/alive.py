49d1a8840e75345792eac067ee0656c15f1bcf04
Anie team <93490359+AnieTeam@users.noreply.github.com>
2021-12-12 15:16:24 +0530

    Add files via upload

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,52 +1,63 @@
-# COPYRIGHT (C) 2021 BY LEGENDX22 AND PROBOYX
+# COPYRIGHT (C) 2021 BY LEGENDX22 AND PROBOYX, ALAIN
 
 """
 (((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
 (((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
 (((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
 (((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-                 MADE BY LEGENDX22 AND PROBOYX
+                 MADE BY LEGENDX AND PROBOY X
+                   DISIGNED BY ALAIN_CHAMPION
                    CREDITS #TEAMLEGEND 
-                PLEASE DON'T REMOVE CREDITS
+                PLEASE DON'T REMOVE THIS LINES
 """
-
 
 from telethon import events, Button, custom
 import re, os
+from LEGENDX import PHOTO, xbot, BOT, VERSION
 from userbot import bot
-from userbot import Anietelethon
-PHOTO = "https://telegra.ph/file/fe58623891803d36979f7.jpg"
-@bot.on(events.InlineQuery(pattern="alive"))
+@xbot.on(events.NewMessage(pattern=("/alive|/start")))
 async def awake(event):
-  builder = event.builder
-  aniex = event.sender.first_name
-  DENVIL_PRO =  "**Aniebot Status**/n/n"
-  DENVIL_PRO += "**ALL System ok**\n/n"
-  DENVIL_PRO += "**➬ Telethon:**{Anietelethon}/n/n"
-  DENVIL_PRO += "**MY Master** {aniex} ☺️/n/n"
-  DENVIL_PRO += "**Full upgrade:**/n/n"
-  DENVIL_PRO += "**Aniebot** {Anieversion}\n\n"
-  DENVIL_PRO += "**thanks for using me**/n/n"
-  BUTTON = [[Button.url("MASTER", "https://t.me/denvil_pro"), Button.url("DEVLOPER", "https://t.me/Denvil_pro")]]
-  BUTTON += [[custom.Button.inline("REPOSITORYS", data="DENVIL_PRO")]]
-  result = builder.photo(
-                    PHOTO,
-                    text=DENVIL_PRO,
-                    buttons=BUTTON,
-                    link_preview=False
-                )
-  await event.answer([result])
+  LEGENDX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  LEGENDX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  LEGENDX += f"{BOT} VERSION : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  LEGENDX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  LEGENDX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  LEGENDX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  LEGENDX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  BUTTON = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  BUTTON += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="LEGENDX")]]
+  await xbot.send_file(event.chat_id, PHOTO, caption=LEGENDX,  buttons=BUTTON)
 
 
 
 
-@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"DENVIL_PRO")))
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"LEGENDX")))
 async def callback_query_handler(event):
-# inline by  LEGENDX22 And DENVIL🔥
-  PROBOYX +=[[Button.url("REPO-ANIE", "https://github.com/ANIETEAM/Aniebots"), Button.url("REPO-GROUP BOT", "https://github.com/Anieteam/AnieRobot")]]
-  PROBOYX +=[[Button.url("DEPLOY-ANIE", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FAnieteam%2FAniebots&template=https%3A%2F%2Fgithub.com%2FAnietEAM%2FAniebots%2FLE"), Button.url("DEPLOY-ANIE GROUP BOT", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2ANIETEAM%2FANIEROBOT&template=https%3A%2F%2Fgithub.com%2FANIETEAM%2FANIEROBOT")]]
-  PROBOYX +=[[Button.url("TUTORIAL", "https://youtu.be/T9ojWwGYBtw"), Button.url("STRING-SESSION", "https://repl.it/@denvilop/Aniebots#main.py")]]
+# inline by LEGENDX22 and PROBOY22 🔥
+  PROBOYX = [[Button.url("REPO-LEGEND", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  PROBOYX +=[[Button.url("DEPLOY-LEGEND", "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2Flegendpack&template=https%3A%2F%2Fgithub.com%2FLEGENDXOP%2Flegendpack")]]
+  PROBOYX +=[[Button.url("TUTORIAL", "https://youtu.be/rGCSSFPsS4Q"), Button.url("STRING-SESSION", "https://repl.it/@legendx22/LEGEND-BOT#main.py")]]
   PROBOYX +=[[Button.url("API_ID & HASH", "https://t.me/usetgxbot"), Button.url("REDIS", "https://redislabs.com")]]
-  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/ANIEBOTS"), Button.url("SUPPORT GROUP", "https://t.me/Aniebotsupports")]]
+  PROBOYX +=[[Button.url("SUPPORT CHANNEL", "https://t.me/LEGENDBOT_OFFICIAL"), Button.url("SUPPORT GROUP", "https://t.me/LEGEND_USERBOT_SUPPORT")]]
   PROBOYX +=[[custom.Button.inline("ALIVE", data="PROBOY")]]
-  await event.edit(text=f"ALL DETAILS OF REPOS", buttons=PROBOYX)
+  await event.edit(text=f"𝙰𝙻𝙻 𝙳𝙴𝚃𝙰𝙸𝙻𝚂 𝙾𝙵 𝚁𝙴𝙿𝙾𝚂", buttons=PROBOYX)
+
+
+@xbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"PROBOY")))
+async def callback_query_handler(event):
+# inline by LEGENDX22 and PROBOY22 🔥
+  LEGENDX = f"ʜᴇʟʟᴏ ᴛʜɪs ɪs  {BOT}\n\n"
+  LEGENDX += "ᴀʟʟ sʏsᴛᴇᴍ ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n\n"
+  LEGENDX += f"{BOT} OS : {VERSION} ʟᴀsᴛᴇsᴛ\n\n"
+  LEGENDX += f"ᴍʏ ᴍᴀsᴛᴇʀ @{bot.me.username} ☺️\n\n"
+  LEGENDX += "ғᴜʟʟʏ ᴜᴘᴅᴀᴛᴇᴅ ʙᴏᴛ\n\n"
+  LEGENDX += "ᴛᴇʟᴇᴛʜᴏɴ : 1.20 LATEST\n\n"
+  LEGENDX += "ᴛʜᴀɴᴋs ғᴏʀ ᴄʜᴇᴄᴋɪɴɢ ᴍᴇ"
+  BUTTONS = [[Button.url("𝙼𝙰𝚂𝚃𝙴𝚁", f"https://t.me/{bot.me.username}"), Button.url(f"{BOT} 𝚁𝙴𝙿𝙾", "https://github.com/LEGENDXOP/LEGEND-BOT")]]
+  BUTTONS += [[custom.Button.inline("𝚁𝙴𝙿𝙾𝚂𝙸𝚃𝙾𝚁𝚈", data="LEGENDX")]]
+  await event.edit(text=LEGENDX, buttons=BUTTONS)
+
+
+@xbot.on(events.NewMessage(pattern=("/repo|#repo")))
+async def repo(event):
+  await xbot.send_message(event.chat, "ʀᴇᴘᴏ ᴏғ ʟᴇɢᴇɴᴅ-ʙᴏᴛ", buttons=[[Button.url("⚜️ ʀᴇᴘᴏ ⚜️", "https://github.com/LEGENDXOP/LEGEND-BOT")]])
